a7182c9f4c5bb3a0eca94efd46ec6b31164a6362
d3xduy <54712464+d3xduy@users.noreply.github.com>
2019-10-20 15:03:06 +0700

    Update course.js

--- js/course.js
+++ js/course.js
@@ -1,9 +1,4 @@
-$(document).ready(function () {
-  $('#dtBasicExample').DataTable({
-    "pagingType": "simple" // "simple" option for 'Previous' and 'Next' buttons only
-  });
-  $('.dataTables_length').addClass('bs-select');
-});
+
  
  $(document).ready(function ()  {
 
